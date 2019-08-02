e8969e386d2cfa328d028914d76f296f03dab9c6
Lee Ryall <lee.ryall@ubisoft.com>
2019-08-02 14:59:03 -0400

    Add python

---
+++ [2019-07-15] Challenge #379 [Easy] Progressive taxation/379.py
@@ -0,0 +1,20 @@
+#start at 14:45
+def tax(income):
+    if income <= 10000:
+        tax = 0
+    elif income > 10000 and income <= 30000:
+        tax = (income - 10000) * 0.1
+    elif income > 30000 and income <= 100000:
+        tax = ((income - 30000) * 0.25) + 2000
+    elif income > 100000:
+        tax = ((income - 100000) * 0.4) + 19500
+    return int(tax)
+
+tax(0)
+tax(10000)
+tax(10009)
+tax(10010)
+tax(12000)
+tax(56789)
+tax(1234567)
+#finish 15:00
